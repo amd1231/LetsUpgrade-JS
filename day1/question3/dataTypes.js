ddf38fdd97cf3646b44883049079e7a048a88e85
Ahmed Morve <46525082+amd1231@users.noreply.github.com>
2020-07-16 13:46:50 +0530

    Add files via upload

---
+++ day1/question3/dataTypes.js
@@ -0,0 +1,40 @@
+//There are eight basic data types in JavaScript. They are as follows:
+
+//1.number:       used to represent number
+
+n=1111;
+console.log(n);
+
+//2.Bigint: used to represent big numbers
+
+m=12345678n
+console.log(m);
+
+//3.string:       represents sequence of characters 
+
+str="This is a string."
+console.log(str);
+
+//4.Boolean:      represents boolean value i.e. either false or true
+
+let isGreater = 4 > 1;
+console.log(isGreater);
+
+//5.Null:         represents null i.e. no value at all
+
+let quantity=null;
+console.log(quantity);
+
+//6.Object:	    represents instance through which we can access members
+var a ={Name:"Ahmed", Age:"20"}
+var b={Name:"Akshata", Age:"20"}
+console.log({a,b});
+
+//7.Array:	    represents group of similar values
+
+var avengers=['Iron Man', 'Thor', 'Hulk']
+console.log(avengers);
+
+//8.Undefined:	represents undefined value
+let age;
+console.log(age);
