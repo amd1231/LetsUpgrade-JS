01aae7512d11722acdcbe3c9038ac7632b3972f7
Ahmed Morve <46525082+amd1231@users.noreply.github.com>
2020-07-19 01:49:46 +0530

    Add files via upload

---
+++ Day2p/q3/ask.js
@@ -0,0 +1,6 @@
+let age = prompt("Enter your age");
+if (age >= 21) {
+  console.log(`you can drink as your age is ${age} `);
+} else {
+    console.log(`you cannot drink as your age is ${age} `);
+}
